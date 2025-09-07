7334c08c7129fcc7fe61f0156660529ce00f35d3
ImStillTapping <delunacarl7@gmail.com>
2025-09-07 20:00:43 +0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -32,8 +32,8 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(35, 35, 48);
     BackgroundColor = Color3.fromRGB(29, 27, 38);
-    AccentColor = Color3.fromRGB(66, 109, 135);
-    OutlineColor = Color3.fromRGB(0, 0, 0);
+    AccentColor = Color3.fromRGB(255, 255, 255);
+    OutlineColor = Color3.fromRGB(255, 255, 255);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
